1092c82afd86c1133da039f521664b4d79b12966
Tristan Matthias <hello@tristanmatthias.com>
2018-04-10 20:50:03 +1000

    Added ButtonGroup to build

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -3,8 +3,8 @@ export {default as Icon} from './Icon';
 export {default as Form} from './Form';
 export {Checkbox, Field} from './Form';
 export {default as Button} from './Button';
+export {default as ButtonGroup} from './ButtonGroup';
 
 
 
-// export * from './ButtonGroup';
 // export * from './Editor';
