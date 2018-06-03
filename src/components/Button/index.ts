9af27ddd708d2e92f1ca2ffa27ca1e9c699a8746
Tristan Matthias <hello@tristanmatthias.com>
2018-06-03 16:31:29 +1000

    Moved button to style decorator

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,14 +1,13 @@
-// tslint:disable function-name
-
 import {html, LitElement} from '@polymer/lit-element';
 import {unsafeHTML} from 'lit-html/lib/unsafe-html';
 import {component, observe, property} from 'polymer3-decorators';
 import Icon from '../Icon';
 import CSS from './button.scss';
-import {bindAttributes} from 'util/decorators';
+import {bindAttributes, style} from 'util/decorators';
 
 @component('zen-button')
 @bindAttributes
+@style(CSS.toString())
 export default class Button extends LitElement {
     @property
     size?: string;
@@ -31,9 +30,10 @@ export default class Button extends LitElement {
         return this.shadowRoot.querySelector('zen-icon') as Icon;
     }
 
+    // tslint:disable-next-line function-name
     _render({icon, size, hollow}: { [key in keyof Button]: any }) {
         return html`
-            ${html`${unsafeHTML(`<style> ${CSS.toString()} </style>`)}`}
+            ${this._style}
             ${icon
                 ? html`<zen-icon type="${icon}" size="${size}" color="${this._iconColor}"></zen-icon>`
                 : ''
