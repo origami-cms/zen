5fe161f5e113050ee2e9c0585d4307db753a8206
Tristan Matthias <hello@tristanmatthias.com>
2018-10-12 16:59:55 +1100

    Fixed issue with input placeholders and default values

--- packages/Input/src/Input.ts
+++ packages/Input/src/Input.ts
@@ -1,48 +1,47 @@
-import {html, LitElement} from '@polymer/lit-element';
-
-import {component, property, bindAttributes, dispatchChange} from '@origamijs/zen-lib/lib/decorators';
-import CSS from './input-css';
+import { dispatchChange } from '@origamijs/zen-lib/lib/decorators';
+import { customElement, html, LitElement, property } from '@polymer/lit-element';
 import { TemplateResult } from 'lit-html';
+import { ifDefined } from 'lit-html/directives/if-defined';
+import CSS from './input-css';
 
-export interface InputProps {
-    placeholder?: string;
-    name?: string;
-    type: string;
-    icon?: string;
-    loading?: boolean;
-    value: string | null;
-    disabled?: string;
-}
 
-@component('zen-input')
-@bindAttributes
+// @ts-ignore
+@customElement('zen-input')
 @dispatchChange()
-export class Input extends LitElement implements InputProps {
+export class Input extends LitElement {
 
-    @property
-    placeholder?: string;
+    @property(
+        {reflect: true, type: String}
+    )
+    placeholder?: string | null;
 
-    @property
-    name?: string;
+    @property(
+        {reflect: true}
+    )
+    name?: string | null;
 
-    @property
-    type: string = 'text';
+    @property(
+        {reflect: true}
+    )
+    type?: string;
 
-    @property
+    @property(
+        {reflect: true, type: String}
+    )
     icon?: string;
 
-    @property
+    @property(
+        {reflect: true, type: Boolean}
+    )
     loading?: boolean;
 
-    @property
-    value: string | null = null;
+    @property()
+    value?: string | null;
 
-    @property
-    disabled?: string;
-
-    static _boundAttributes = [
-        'placeholder', 'name', 'type', 'icon', 'loading', 'disabled'
-    ];
+    @property(
+        {reflect: true, type: Boolean}
+    )
+    disabled?: boolean;
 
     constructor() {
         super();
@@ -52,21 +51,19 @@ export class Input extends LitElement implements InputProps {
 
     render(): TemplateResult {
         const { icon, loading, type, placeholder, disabled, value } = this;
+
         const v = value || '';
 
         return html`
             ${CSS}
-            ${icon
-                ? html`<zen-icon type="${icon}".color="grey-300"></zen-icon>`
-                : ''
-            }
+            ${icon ? html`<zen-icon type="${icon}".color="grey-300"></zen-icon>` : ''}
 
             <input
-                .value="${v}"
-                type="${type}"
-                placeholder="${placeholder}"
-                .disabled="${disabled}"
-                @input="${this._handleInput}"
+                .value=${v}
+                .type=${type}
+                placeholder=${ifDefined(placeholder)}
+                .disabled=${disabled}
+                @input=${this._handleInput}
             />
 
             ${loading
